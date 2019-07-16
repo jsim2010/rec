5a1ed1cc68146d281987b12c459dc61b0728b04e
Justin Simerly <justinsimerly1992@gmail.com>
2019-07-16 09:11:38 -0400

    Add Tokens::name_parse()

--- src/lib.rs
+++ src/lib.rs
@@ -216,6 +216,17 @@ impl Tokens<'_> {
     pub fn name_str(&self, name: &str) -> Option<&str> {
         self.name(name).map(|m| m.as_str())
     }
+
+    /// Returns the `F` parsed from the [`str`] of the [`Match`] for the given capture name.
+    ///
+    /// If no match is found, returns [`None`]. If error occurs while parsing match, returns
+    /// `Some(Err(Error))`.
+    pub fn name_parse<F>(&self, name: &str) -> Option<Result<F, <F as FromStr>::Err>>
+    where
+        F: FromStr,
+    {
+        self.name_str(name).map(str::parse::<F>)
+    }
 }
 
 impl<'t> Deref for Tokens<'t> {
