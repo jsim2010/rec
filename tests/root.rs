ab2ce1abbc043bc5da9436c8a60f4f1e77bc0830
Justin Simerly <justinsimerly1992@gmail.com>
2019-08-27 16:53:31 -0400

    Dumping github context

--- tests/root.rs
+++ tests/root.rs
@@ -1,23 +1,4 @@
-use std::{fs, process::Command};
 use version_sync::assert_markdown_deps_updated;
-
-/// The README.md shall match the crate documentation.
-#[test]
-fn readme() {
-    let actual_readme = fs::read_to_string("README.md")
-        .expect("unable to read 'README.md'")
-        .replace('\r', "");
-    let desired_readme = String::from_utf8_lossy(
-        &Command::new("cargo")
-            .arg("readme")
-            .output()
-            .expect("failed to execute command")
-            .stdout,
-    )
-    .into_owned();
-
-    assert_eq!(actual_readme, desired_readme);
-}
 
 /// The crate version listed in the README file shall match the current version.
 #[test]
