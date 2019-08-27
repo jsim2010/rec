152fa2e5a617cb9911dabb8b53cf2f6663229a1b
Justin Simerly <justinsimerly1992@gmail.com>
2019-08-27 17:27:26 -0400

    Add specifications to tests (#73)
    * Adding specs to tests
    
    * Start adding specs for repetition
    
    * Update rust_check action
    
    * Add build and test to rust_check action
    
    * Update rust to latest version in rust_check action
    
    * Update to run one job for testing
    
    * Fix invalid rust_check action
    
    * Dumping github context
    
    * Add conditional for update job
    
    * Fix invalid step
    
    * Fix invalid string literal in rust_pr workflow
    
    * Dump labels
    
    * Add object filter for label names
    
    * Add object filter for pull_request label
    
    * Fixing conditionals

--- tests/path.rs
+++ tests/path.rs
@@ -3,16 +3,17 @@ use rec::rec;
 #[rec]
 const DIGIT: Rec = '0'..'9';
 
+/// A path expression that resolves to a const [`&str`] shall be equal to the resolved value.
 #[test]
-fn single_call() {
+fn single_path() {
     #[rec]
     const NEW_DIGIT: Rec = DIGIT;
-    assert_eq!(DIGIT, "[0-9]");
+    assert_eq!(NEW_DIGIT, "[0-9]");
 }
 
 #[test]
 #[ignore] // Waiting for implementation of const variable concatentation.
-fn call_repeat() {
+fn repeat_path() {
     //#[rec]
     //const SOME_DIGITS: Rec = [DIGIT; 1..];
     //assert_eq!(SOME_DIGITS, "[0-9]+");
@@ -20,7 +21,7 @@ fn call_repeat() {
 
 #[test]
 #[ignore] // Waiting for implementation of const variable concatentation.
-fn alternate_with_call() {
+fn alternate_with_path() {
     //#[rec]
     //const A_OR_DIGIT: Rec = 'a' | DIGIT;
     //assert_eq!(A_OR_DIGIT, "a|[0-9]");
--- tests/root.rs
+++ tests/root.rs
@@ -1,5 +1,6 @@
 use version_sync::assert_markdown_deps_updated;
 
+/// The crate version listed in the README file shall match the current version.
 #[test]
 fn readme_deps_updated() {
     assert_markdown_deps_updated!("README.md");
