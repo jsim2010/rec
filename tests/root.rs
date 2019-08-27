9e18d5c1c5e9eb8dbce206ef69619a12ee56658c
Justin Simerly <justinsimerly1992@gmail.com>
2019-08-27 16:00:38 -0400

    Update rust to latest version in rust_check action

--- tests/root.rs
+++ tests/root.rs
@@ -1,26 +1,6 @@
 use std::{fs, process::Command};
 use version_sync::assert_markdown_deps_updated;
 
-/// The name of the program shall be "rec".
-#[test]
-fn name() {
-    #[allow(unused_imports)] // Test only needs to confirm that library with correct name exists.
-    use rec;
-}
-
-/// [`rec`] shall be used as a library that provides the following:
-///     1) "rec" = an attribute procedural macro that can be applied to a constant item with type [`&str`]. The procedural macro defines a constant [`&str`] with the given identifier.
-#[test]
-fn interface() {
-    use rec::rec;
-
-    #[rec]
-    const TEST: &str = ' ';
-
-    /// Assert TEST is const &str.
-    const _: &str = TEST;
-}
-
 /// The README.md shall match the crate documentation.
 #[test]
 fn readme() {
