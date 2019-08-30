fc8e1a6a9ff55f54394e1d50b68a2e417c91a66a
Justin Simerly <justinsimerly1992@gmail.com>
2019-08-30 06:41:31 -0400

    Finish adding specs to tests (#80)
    * Finish adding specs for reptitions
    
    * Add echo of conditions
    
    * Fix pr workflow

--- tests/repetitions.rs
+++ tests/repetitions.rs
@@ -1,6 +1,6 @@
 use rec::rec;
 
-/// A repetition with a range full shall repeat zero or more times.
+/// A repetition with a range full shall repeat 0 or more times.
 #[test]
 fn repeat_zero_or_more() {
     #[rec]
@@ -8,7 +8,7 @@ fn repeat_zero_or_more() {
     assert_eq!(ZERO_OR_MORE, "x*");
 }
 
-/// A repetition with a lazy range full shall lazily repeat zero or more times.
+/// A repetition with a lazy range full shall lazily repeat 0 or more times.
 #[test]
 fn lazy_repeat_zero_or_more() {
     #[rec]
@@ -24,6 +24,7 @@ fn repeat_one_or_more() {
     assert_eq!(ONE_OR_MORE, "x+");
 }
 
+/// A repetition with a lazy range from 1 shall lazily repeat 1 or more times.
 #[test]
 fn lazy_repeat_one_or_more() {
     #[rec]
@@ -31,6 +32,7 @@ fn lazy_repeat_one_or_more() {
     assert_eq!(LAZY_ONE_OR_MORE, "x+?");
 }
 
+/// A repetition with a range to inclusive 1 shall repeat 0 or 1 times.
 #[test]
 fn repeat_zero_or_one() {
     #[rec]
@@ -38,6 +40,7 @@ fn repeat_zero_or_one() {
     assert_eq!(ZERO_OR_ONE, "x?");
 }
 
+/// A repetition with a lazy range to inclusive 1 shall lazily repeat 0 or 1 times.
 #[test]
 fn lazy_repeat_zero_or_one() {
     #[rec]
@@ -45,6 +48,7 @@ fn lazy_repeat_zero_or_one() {
     assert_eq!(LAZY_ZERO_OR_ONE, "x??");
 }
 
+/// A repetition with a range inclusive shall repeat a number of times within the given range.
 #[test]
 fn repeat_at_least_and_at_most() {
     #[rec]
@@ -52,6 +56,8 @@ fn repeat_at_least_and_at_most() {
     assert_eq!(RANGE, "x{3,5}");
 }
 
+/// A repetition with a lazy range inclusive shall lazily repeat a number of times within the given
+/// range.
 #[test]
 fn lazy_repeat_at_least_and_at_most() {
     #[rec]
@@ -59,6 +65,8 @@ fn lazy_repeat_at_least_and_at_most() {
     assert_eq!(LAZY_RANGE, "x{3,5}?");
 }
 
+/// A repetition with a range from a number greater than 1 shall repeat at least that number of
+/// times.
 #[test]
 fn repeat_at_least() {
     #[rec]
@@ -66,6 +74,8 @@ fn repeat_at_least() {
     assert_eq!(AT_LEAST, "x{2,}");
 }
 
+/// A repetition with a lazy range from a number greater than 1 shall lazily repeat at least that
+/// number of times.
 #[test]
 fn lazy_repeat_at_least() {
     #[rec]
@@ -73,20 +83,7 @@ fn lazy_repeat_at_least() {
     assert_eq!(LAZY_AT_LEAST, "x{2,}?");
 }
 
-#[test]
-fn repeat_at_most() {
-    #[rec]
-    const AT_MOST: Rec = ['x'; 0..=4];
-    assert_eq!(AT_MOST, "x{0,4}");
-}
-
-#[test]
-fn lazy_repeat_at_most() {
-    #[rec]
-    const LAZY_AT_MOST: Rec = ['x'; lazy(0..=4)];
-    assert_eq!(LAZY_AT_MOST, "x{0,4}?");
-}
-
+/// A repetition with a literal integer shall repeat that number of times.
 #[test]
 fn repeat_exact() {
     #[rec]
@@ -94,6 +91,8 @@ fn repeat_exact() {
     assert_eq!(EXACT, "x{3}");
 }
 
+/// A repetition with a compound [`rec expression`] shall repeat the entire [`rec expression`] as
+/// one item.
 #[test]
 fn repeat_compound_expr() {
     #[rec]
